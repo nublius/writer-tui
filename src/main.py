cb765aee0319209e8a43079be679c8b13c6ae0d6
nublius <184036331+nublius@users.noreply.github.com>
2026-01-12 14:13:12 -0800

    basic layout of window

---
+++ src/main.py
@@ -0,0 +1,29 @@
+import pytermgui as ptg
+
+def main():
+    input_field = ptg.InputField(
+        prompt="> "
+    )
+
+    content_box = ptg.Container(
+        ptg.Label("hello world")
+    )
+
+    layout = ptg.Container(
+        content_box,
+        input_field
+    )
+
+    window = ptg.Window(
+        layout,
+        title="Writer TUI",
+        expand=True
+    )
+
+
+    manager = ptg.WindowManager()
+    manager.add(window)
+
+    manager.run()
+
+main()
