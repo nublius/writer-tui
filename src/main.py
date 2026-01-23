7d54bc5c8587570e72c362423ba8314ca63b221c
nublius <184036331+nublius@users.noreply.github.com>
2026-01-23 13:08:34 -0800

    build widgets for textdisplay and input

--- src/main.py
+++ src/main.py
@@ -1,5 +1,13 @@
 from textual.app import App
-from textual.widgets import Button, Footer, Header, Input, Markdown
+from textual.widgets import Button, Footer, Header, Input, Markdown, Static
+
+class TextDisplay(Static):
+    def compose(self):
+        yield Markdown("Hello world", id="markdown")
+
+class InputBox(Static):
+    def compose(self):
+        yield Input(id="input")
 
 class Writer(App):
     CSS_PATH = "styles.css"
@@ -7,8 +15,8 @@ class Writer(App):
     def compose(self):
         yield Header()
         yield Footer()
-        yield Markdown("Hello World")
-        yield Input()
+        yield TextDisplay(id="text_display")
+        yield InputBox(id="input_box")
 
 if __name__ == "__main__":
     app = Writer()
