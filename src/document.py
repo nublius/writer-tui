16187fad31061e968c251cc9976ee57a684f21ef
nublius <184036331+nublius@users.noreply.github.com>
2026-01-26 13:05:41 -0800

    switch to Document class instead of markdown.py

---
+++ src/document.py
@@ -0,0 +1,22 @@
+import os
+
+class Document:
+    def __init__(self, file_name, folder="dest"):
+        # Absolute path to destination folder
+        base_dir = os.path.dirname(os.path.abspath(__file__))
+        self.folder = os.path.join(base_dir, folder)
+        os.makedirs(self.folder, exist_ok=True)
+
+        # Full path to file
+        self.file_path = os.path.join(self.folder, filename)
+
+        # Check if file exists(empty if new)
+        self._ensure_empty()
+
+    def _ensure_empty(self):
+        if not os.path.exists(self.file_path):
+            open(self.file_path, "x").close()
+
+    def append(self, line):
+        with open(self.file_path, "a") as f:
+            f.write(line + "\n")
